babb7ec92d5bd47cc658de5486555a36458016a1
pramodhbn <pramodh.nanjayya@gmail.com>
2022-03-04 14:25:07 +0000

    Added Lambda converters

--- main.kt
+++ main.kt
@@ -10,4 +10,21 @@ fun main(args: Array<String>) {
   println(greeting())
   val myLambda: () -> Unit = { println("Hello Lambda")}
   myLambda()
+  val converter = {x: Double -> x*x}
+  println(convert(100.25, converter))
+  convert(20.0) { it * 20.0 + 6.1}
+  convertFive { it * 4.5 + 10000.09999}
+}
+
+fun convert(x: Double, converter: (Double)-> Double): Double {
+  // code to convert the Double
+  val result = converter(x)
+  println("$x is convereted to $result")
+  return result
+}
+
+fun convertFive(converter: (Int) -> Double): Double {
+  val result = converter(5)
+  println("5 is converted to $result")
+  return result
 }
