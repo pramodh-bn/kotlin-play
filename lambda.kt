98b7f00eef8f724963eb6a4fd6d0ff0dd3edfca2
pramodhbn <pramodh.nanjayya@gmail.com>
2022-03-05 23:18:51 +0000

    Added getConversionLambda

--- lambda.kt
+++ lambda.kt
@@ -4,6 +4,18 @@ fun unless(condition:Boolean, code: () -> Unit) {
   }
 }
 
+fun getConversionLambda(str: String): (Double) -> Double {
+  if(str == "CentigradeToFahrenheit") {
+    return { it * 1.8 + 32 }
+  } else if(str == "KgsToPounds") {
+    return { it * 2.20463}
+  } else if(str == "PoundsToUSTons") {
+    return { it / 2000.0 }
+  } else {
+    return { it }
+  }
+}
+
 fun main() {
   val options = arrayOf("Red", "Amber", "Green")
   var crossWalk = options[(Math.random() * options.size).toInt()]
@@ -13,4 +25,7 @@ fun main() {
   unless(crossWalk == "Green") {
     println("Stop")
   }
+
+  val pounds = getConversionLambda("KgsToPounds")(2.5)
+  println("These are the $pounds")
 }
