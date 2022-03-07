de3f161ee5973c7214b939a570ee2cb70d46db90
pramodhbn <pramodh.nanjayya@gmail.com>
2022-03-07 14:01:26 +0000

    Added maxBy and minBy for groceries collections

--- builtinFn.kt
+++ builtinFn.kt
@@ -1,11 +1,18 @@
 data class Grocery(val name: String, val category: String, val unit: String, val unitPrice: Double, val quantity: Int) 
 
 fun main() {
-  val grocerties = listOf(
+  val groceries = listOf(
     Grocery("Tomatoes", "Vegetable", "lb", 3.0, 3),
     Grocery("Mushrooms", "Vegetable", "lb", 4.0, 1),
     Grocery("Bagels", "Bakery", "Pack", 1.5, 2),
     Grocery("Olive Oil", "Pantry", "Bottle", 6.0, 1),
     Grocery("Ice Cream", "Frozen", "Pack", 3.0, 2)
   )
+
+  val ints = listOf(1, 2, 3, 4)
+  val maxInt = ints.maxOrNull()
+  println("This is the max of $ints ---> $maxInt")
+  val highestUnitPrice = groceries.maxByOrNull { it.unitPrice }
+  val lowestQuantity = groceries.minByOrNull { it.quantity }
+  println("$highestUnitPrice --- $lowestQuantity")
 }
